c5631361cbc0d3c92dc493b4f6cfeeb1232bb201
Theodor Schaathun <thesch@gvgs.no>
2026-03-04 13:13:16 +0100

    Fortsettet med å gjøre bakken solid

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import math
 import random
+import time
+
 
 pygame.init()
 
@@ -11,8 +13,10 @@ clock = pygame.time.Clock()
 delta_time = 0.1
 x = 304
 y = -480
+speedY = 0
 cam_y = y
 collision = False
+font = pygame.font.Font(None, size=30)
 
 # Temporary creation of stone
 stone = pygame.image.load('img/stone-v1.png').convert_alpha()
@@ -51,7 +55,6 @@ def make_terrain():
 # Drawing of terrain
 terrain = make_terrain()
 def draw_terrain(screen, terrain, player_y):
-    grounds = []
     idx = 0
     x = 0
     y = 0 - player_y
@@ -60,32 +63,97 @@ def draw_terrain(screen, terrain, player_y):
         for i in range(20):
             if terrain[idx] != "":
                 screen.blit(terrain[idx], (x,y))
-                ground = pygame.Rect(x, y, 32, 32)
-                pygame.draw.rect(screen, (0, 255, 0), ground)
-                grounds.append(ground)
+                if idx == 0:
+                    first_block = font.render(f"First Block: {x}, {math.floor(y)}", True, (255, 255, 255))
+
             idx += 1
             x += 32
         x = 0
         y += 32
-    return(grounds)
+    return(first_block)
+
+
+def squares(player_x, player_y):
+    if player_y > -240:
+        global temp1
+        global temp2
+        global temp3
+        global temp4
+        global temp5
+        global temp6
+        global temp7
+        global temp8
+        global temp9
+
+        idx = 0
+        y = 0
+        
+
+        # Top right
+        temp1 = pygame.Rect(math.floor(player_x/32)*32-32, 192, 32, 32)
+        pygame.draw.rect(screen, (0, 0, 255), temp1)
+        # Top center
+        temp2 = pygame.Rect(math.floor(player_x/32)*32, 192, 32, 32)
+        pygame.draw.rect(screen, (0, 0, 255), temp2)
+        # Top left
+        temp3 = pygame.Rect(math.floor(player_x/32)*32+32, 192, 32, 32)
+        pygame.draw.rect(screen, (0, 0, 255), temp3)
+        # Right
+        temp4 = pygame.Rect(math.floor(player_x/32)*32-32, 224, 32, 32)
+        pygame.draw.rect(screen, (0, 0, 255), temp4)
+        # Center
+        temp5 = pygame.Rect(math.floor(player_x/32)*32, 224, 32, 32)
+        pygame.draw.rect(screen, (0, 0, 255), temp5)
+        # Left
+        temp6 = pygame.Rect(math.floor(player_x/32)*32+32, 224, 32, 32)
+        pygame.draw.rect(screen, (0, 0, 255), temp6)
+        # Bottom right
+        temp7 = pygame.Rect(math.floor(player_x/32)*32-32, 256, 32, 32)
+        pygame.draw.rect(screen, (0, 0, 255), temp7)
+        #Bottom center
+        temp8 = pygame.Rect(math.floor(player_x/32)*32, 256, 32, 32)
+        pygame.draw.rect(screen, (255, 0, 255), temp8)
+        # Bottom left
+        temp9 = pygame.Rect(math.floor(player_x/32)*32+32, 256, 32, 32)
+        pygame.draw.rect(screen, (0, 0, 255), temp9)
+    #print(collision)
     
 print(len(terrain)/20)
+hitbox = pygame.Rect(x, 224, 32, 32)
+first_block = font.render(f"Not spawned", True, (255, 255, 255))
+squares(0,0)
 while running:
     screen.fill((0,0,0))
 
-    ground = draw_terrain(screen, terrain, cam_y)
+    first_block=draw_terrain(screen, terrain, cam_y)
     screen.blit(player, (x, 224))
-    speedY = 50
-    if not collision:
-        y += speedY * delta_time
+
+    y_pos = font.render(f"Y pos: {y}", True, (255, 255, 255))
+    screen.blit(y_pos,(4,4))
+    
+    y += speedY * delta_time
+    hitbox = pygame.Rect(x, 226, 32, 32)
+    collision = hitbox.colliderect(temp8)
+
+    #print(collision)
+    if collision:
+        speedY = 0
+    elif speedY != 50:
+        speedY = speedY+1
+        
+    y_vel = font.render(f"Y vel: {speedY}", True, (255, 255, 255))
+    screen.blit(first_block,(4,20))
+    screen.blit(y_vel,(4,36))
+
+
+    squares(x,y)
     cam_y = y
 
     hitbox = pygame.Rect(x, 224, 32, 32)
-    #collision = hitbox.colliderect(ground)
-    #print(collision)
-    for i in range(len(ground)):
-        collision = hitbox.colliderect(ground[i])
-        print(collision)
+    
+    #for i in range(len(ground)):
+        #collision = hitbox.colliderect(ground[i])
+        #print(collision)
     pygame.draw.rect(screen, (255, 0, collision), hitbox)
 
     for event in pygame.event.get():
@@ -95,12 +163,12 @@ while running:
         if pressed[pygame.K_SPACE]:
             print("spaced")
             terrain = make_terrain()
-            y = -640
+            y = -480
             cam_y = y
+            speedY = 0
         if pressed[pygame.K_w]:
             print("w")
-            y -=1
-            cam_y = y
+            speedY = -10
         if pressed[pygame.K_s]:
             print("s")
             y += 1
