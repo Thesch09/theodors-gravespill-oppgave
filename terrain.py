17be66ba864a59db3333c48bea5dd56447e82ae2
Theodor Schaathun <thesch09@gvgs.no>
2026-03-15 17:04:54 +0100

    Gjorde terrain.py mye kortere og mye enklere å legge til noe nytt. Det tar litt tid å laste inn, men det går greit

--- terrain.py
+++ terrain.py
@@ -3,22 +3,96 @@ import random
 import math
 # Generate the terrain
 def make_terrain(depth):
+    class ore:
+        def __init__(self, type, name, hardness, value, damage, minHeight = None, maxHeight = None, chance = None, uniqueOre = None):
+            self.type = type
+            self.name = name
+            self.hardness = hardness
+            self.value = value
+            self.damage = damage
+            self.minHeight = minHeight
+            self.maxHeight = maxHeight
+            self.chance = chance
+            self.uniqueOre = uniqueOre
+
+    def addOre(ore, terrain, location):
+        if ore.type == "rock":
+            terrain[location].ore = ore.name
+            terrain[location].hardness = ore.hardness
+            terrain[location].value = ore.value
+            terrain[location].damage = ore.damage
+        elif ore.type == "mineral":
+            terrain[location].extra = ore.name
+            terrain[location].hardness += ore.hardness
+            terrain[location].value += ore.value
+            terrain[location].damage += ore.damage
+            if ore.uniqueOre != None:
+                terrain[location].uniqueOre = ore.uniqueOre
+        else:
+            print("UNDIFINED RESOURCE TYPE")
+    
+    def addToTerrain(list, terrain):
+        rockTions = [] # A portmanteau of rock and options
+        for i in list:
+            if i.minHeight != None and i.maxHeight != None:
+                if height >= i.minHeight and height <= i.maxHeight:
+                    rockTions.append(i)
+        print(len(rockTions))
+        if len(rockTions) >= 1:
+            selectedRockTion = rockTions[random.randint(0, len(rockTions)-1)]
+            if i.type == "rock" or i.type == "mineral" and random.randint(1, selectedRockTion.chance) == 1 and terrain[-1].ore != "" and terrain[-1].ore != "Dirt":
+                addOre(selectedRockTion, terrain, -1)
+        else:
+            pass
+
+    # Creation of stones
+    # rockName = ore("rock", "Name of the rock for drawing it", hardness, value, damage, minimum height, maximum height)
+    dirt = ore("rock", "Dirt", 10, 1, 0, 0, 2)
+    stone = ore("rock", "Stone", 25, 2, 1, 2, 74)
+    bluestone = ore("rock", "Bluestone", 50, 7, 3, 73, 149)
+    redstone = ore("rock", "Redstone", 200, 50, 7, 147, 299)
+    bloodstone = ore("rock", "Bloodstone", 500, 150, 15, 298, 449)
+    abyssmarine = ore("rock", "Abyssmarine", 1000, 350, 35, 448, 749)
+    spaceStone = ore("rock", "Space Stone", 1500, 500, 50, 748, 999)
+    space = ore("rock", "Space", 5000, 1000, 100, 998, 1250)
+
+    rocks = [dirt, stone, bluestone, redstone, bloodstone, abyssmarine, spaceStone, space]
+
+    # Creation of minerals
+    # mineralName = ore("mineral", "Name of the mineral for drawing it", hardness, value, damage, minimum height, maximum height, chance for it to appear)
+    coal = ore("mineral", "Coal", 10, 2, 1, 4, 50, 15)
+    iron = ore("mineral", "Iron", 20, 7, 1, 15, 50, 20)
+    uniqueOre1 = ore("mineral", "Unique Ore", 100, 15, 5, 0, depth, 100, 1)
+    uniqueOre2 = ore("mineral", "Big Unique Ore", 500, 100, 30, 0, depth, 200, 2)
+    uniqueOre3 = ore("mineral", "Large Unique Ore", 1000, 1000, 90, 0, depth, 300, 3)
+
+    ores = [coal, iron, uniqueOre1, uniqueOre2, uniqueOre3]
+
+    # Special stuff
+    magma = ore("rock","Magma", 70, 50, 50)
+    grass = ore("mineral", "Grass", 5, 0, 0)
+
+    # I am le genius
+
     terrain = []
     height = 0
     for i in range(depth):
         for i in range(20):
             class block:
-                x = 0
-                y = 0
+                # Can be modified by ore/mineral
                 ore = 'Dirt'
-                rect = pygame.Rect(x,y,32,32)
-                hardness = 10
-                mined = False
                 extra = ''
+                hardness = 10
                 value = 1
                 damage = 0
-                health = hardness
+                # Exclusivly by unique ore
                 uniqueOre = 0
+                # Can't be modified by ore/mineral
+                x = 0
+                y = 0
+                rect = pygame.Rect(x,y,32,32)
+                mined = False
+                health = hardness
                 deep = height
             terrain.append(block)
             terrain[-1].x = i*32
@@ -26,182 +100,20 @@ def make_terrain(depth):
             terrain[-1].rect = pygame.Rect(terrain[-1].x,terrain[-1].y,32,32)
 
             # Base rock
-            if height < 3:
-                terrain[-1].ore = 'Dirt'
-                terrain[-1].value = 1
-                terrain[-1].damage = 0
-            if height == 2 and random.randint(1,2) == 2:
-                terrain[-1].ore = 'Stone'
-                terrain[-1].hardness = 25
-                terrain[-1].value = 2
-                terrain[-1].damage = 1
-            if height >= 3:
-                terrain[-1].ore = 'Stone'
-                terrain[-1].hardness = 25
-                terrain[-1].value = 2
-                terrain[-1].damage = 1
-            if height > 73:
-                if random.randint(1,2) == 2:
-                    terrain[-1].ore = 'Bluestone'
-                    terrain[-1].hardness = 50
-                    terrain[-1].value = 7
-                    terrain[-1].damage = 3
-                else:
-                    terrain[-1].ore = 'Stone'
-                    terrain[-1].hardness = 25
-                    terrain[-1].value = 2
-                    terrain[-1].damage = 3
-            if height > 75:
-                terrain[-1].ore = 'Bluestone'
-                terrain[-1].hardness = 50
-                terrain[-1].value = 7
-                terrain[-1].damage = 3
-            if height > 148:
-                if random.randint(1,2) == 1:
-                    terrain[-1].ore = 'Redstone'
-                    terrain[-1].hardness = 200
-                    terrain[-1].value = 50
-                    terrain[-1].damage = 7
-                else:
-                    terrain[-1].ore = 'Bluestone'
-                    terrain[-1].hardness = 50
-                    terrain[-1].value = 7
-                    terrain[-1].damage = 3
-            if height > 150:
-                terrain[-1].ore = 'Redstone'
-                terrain[-1].hardness = 200
-                terrain[-1].value = 50
-                terrain[-1].damage = 7
-            if height > 298:
-                if random.randint(1,2) == 1:
-                    terrain[-1].ore = 'Bloodstone'
-                    terrain[-1].hardness = 500
-                    terrain[-1].value = 150
-                    terrain[-1].damage = 15
-                else:
-                    terrain[-1].ore = 'Redstone'
-                    terrain[-1].hardness = 200
-                    terrain[-1].value = 50
-                    terrain[-1].damage = 7
-            if height > 300:
-                terrain[-1].ore = 'Bloodstone'
-                terrain[-1].hardness = 500
-                terrain[-1].value = 150
-                terrain[-1].damage = 15
-            if height > 448:
-                if random.randint(1,2) == 1:
-                    terrain[-1].ore = 'Abyssmarine'
-                    terrain[-1].hardness = 1000
-                    terrain[-1].value = 350
-                    terrain[-1].damage = 35
-                else:
-                    terrain[-1].ore = 'Bloodstone'
-                    terrain[-1].hardness = 500
-                    terrain[-1].value = 150
-                    terrain[-1].damage = 15
-            if height > 450:
-                terrain[-1].ore = 'Abyssmarine'
-                terrain[-1].hardness = 1000
-                terrain[-1].value = 350
-                terrain[-1].damage = 35
-            if height > 748:
-                if random.randint(1,2) == 1:
-                    terrain[-1].ore = 'Space Stone'
-                    terrain[-1].hardness = 1500
-                    terrain[-1].value = 500
-                    terrain[-1].damage = 50
-                else:
-                    terrain[-1].ore = 'Abyssmarine'
-                    terrain[-1].hardness = 1000
-                    terrain[-1].value = 350
-                    terrain[-1].damage = 35
-            if height > 750:
-                terrain[-1].ore = 'Space Stone'
-                terrain[-1].hardness = 1500
-                terrain[-1].value = 500
-                terrain[-1].damage = 50
-            if height > 998:
-                if random.randint(1,2) == 1:
-                    terrain[-1].ore = 'Space'
-                    terrain[-1].hardness = 5000
-                    terrain[-1].value = 1000
-                    terrain[-1].damage = 100
-                else:
-                    terrain[-1].ore = 'Space Stone'
-                    terrain[-1].hardness = 1500
-                    terrain[-1].value = 500
-                    terrain[-1].damage = 50
-            if height > 1000:
-                terrain[-1].ore = 'Space'
-                terrain[-1].hardness = 5000
-                terrain[-1].value = 1000
-                terrain[-1].damage = 100
-            if height > 1199:
-                if random.randint(1,4) == 1:
-                    terrain[-1].ore = ''
-                    terrain[-1].mined = True
-                else:
-                    terrain[-1].ore = 'Space'
-                    terrain[-1].hardness = 5000
-                    terrain[-1].value = 1000
-                    terrain[-1].damage = 100
-            if height > 1248:
-                if random.randint(1,2) == 1:
-                    terrain[-1].ore = ''
-                    terrain[-1].mined = True
-                else:
-                    terrain[-1].ore = 'Space'
-                    terrain[-1].hardness = 5000
-                    terrain[-1].value = 1000
-                    terrain[-1].damage = 100
+            addToTerrain(rocks, terrain)
+            if height >= 175 and height <= 700 and random.randint(1, 50) == 1:
+                addOre(magma, terrain, -1)
+            if height >= 1200 and random.randint(1,4) == 1:
+                terrain[-1].ore = ""
+                terrain[-1].mined = True
+            if height >= 1248 and random.randint(1,2) == 1:
+                terrain[-1].ore = ""
+                terrain[-1].mined = True
 
-            if height > 174 and random.randint(1, 50) == 1 and height < 700:
-                terrain[-1].ore = 'Magma'
-                terrain[-1].hardness = 70
-                terrain[-1].value = 50
-                terrain[-1].damage = 50
-            
-
-            if height == 0 and i == 9 or height == 0 and i == 10:
-                terrain[-1].ore = 'Bluestone'
-                terrain[-1].hardness = 50
-                terrain[-1].value = 7
-                terrain[-1].damage = 3
-            
-            # Ore
+            # Minerals
+            addToTerrain(ores, terrain)
             if height == 0:
-                terrain[-1].extra = 'Grass'
-                terrain[-1].hardness += 5
-            if height > 3 and height < 50 and random.randint(1,15) == 1 and terrain[-1].extra == '':
-                terrain[-1].extra = 'Coal'
-                terrain[-1].hardness += 10
-                terrain[-1].value += 2
-                terrain[-1].damage += 1
-            if height > 15 and height < 50 and random.randint(1,20) == 1 and terrain[-1].extra == '':
-                terrain[-1].extra = 'Iron'
-                terrain[-1].hardness += 20
-                terrain[-1].value += 7
-                terrain[-1].damage += 1
-            
-            # Unique ore
-            if terrain[-1].extra == '' and terrain[-1].ore != 'Dirt' and terrain[-1].ore != '' and random.randint(1,100) == 1:
-                terrain[-1].extra = 'Unique Ore'
-                terrain[-1].hardness += 100
-                terrain[-1].uniqueOre = 1
-                terrain[-1].damage += 5
-                terrain[-1].value += 15
-            if terrain[-1].extra == '' and terrain[-1].ore != 'Dirt' and terrain[-1].ore != '' and random.randint(1,200) == 1:
-                terrain[-1].extra = 'Big Unique Ore'
-                terrain[-1].hardness += 500
-                terrain[-1].uniqueOre = 2
-                terrain[-1].damage += 30
-                terrain[-1].value += 100
-            if terrain[-1].extra == '' and terrain[-1].ore != 'Dirt' and terrain[-1].ore != '' and random.randint(1,300) == 1:
-                terrain[-1].extra = 'Large Unique Ore'
-                terrain[-1].hardness += 1000
-                terrain[-1].uniqueOre = 3
-                terrain[-1].damage += 90
-                terrain[-1].value += 1000
+                addOre(grass, terrain, -1)
 
             # Extra health the deeper you go
             terrain[-1].hardness += 5*math.floor(((height-10)/10))
